c8a800933886405c5e56a2add9087904961ad300
benediktavicius <l.benediktavicius@gmail.com>
2017-02-10 17:59:31 +0200

    Add files via upload

--- global/header.php
+++ global/header.php
@@ -1,16 +1,19 @@
 <nav class="navbar">
 	<div class="container">
 
-		<a href="../main/page.php">
-			<img src="../images/ebook.png">
-		</a>
-		
-		<form class="search" action="../main/search.php" method="get">
-			<input type="text" name="keywords" autocomplete="on" placeholder="Search for a book...">			
-			<button type="submit">
-				<span class="glyphicon glyphicon-search"></span>
-			</button>
-		</form>
+		<div class="col-md-4">
+			<a href="../main/page.php">
+				<img src="../images/ebook.png">
+			</a>
+		</div>
+		<div class="input-group navbar-right col-md-8">
+			<form class="search" action="../main/search.php" method="get">
+				<input type="text" class="form-control" name="keywords" placeholder="Search for a book...">			
+				<span class="input-group-btn">
+					<button class="btn btn-default" type="submit"><span class="glyphicon glyphicon-search" aria-hidden="true"></span></button>
+				</span>
+			</form>
+		</div>
 	</div>
 </nav>
 
